6475fc69a366b633dcf6ae8838ba55c0d362eb72
Carla Sofia Centeleghe <81233915+Carla-Sofia-Centeleghe@users.noreply.github.com>
2023-11-21 20:18:22 -0300

    agreago test mas

--- Persona/test.py
+++ Persona/test.py
@@ -10,10 +10,13 @@ class TestFinal(unittest.TestCase):
         self.assertEqual(persona.apellido, 'Rogers')
         self.assertEqual(persona.edad, 25)
 
-    def test_menor_edad_exception(self):
+    def test_menor_edad_exception_1(self):  #final 02/08/2023
         with self.assertRaises(MenorEdadException):
             Persona('Star', 'Fire', 10)
 
+    def test_menor_edad_exception_2(self): #final 20/09/2023
+        with self.assertRaises(MenorEdadException):
+           Persona('Natasha', 'Romanof', 17)
 
 if __name__ == '__main__':
     unittest.main()
