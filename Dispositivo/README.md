0a7fcb1c65828c82de43e1a1fc5bf589d3490c17
Carla Sofia Centeleghe <81233915+Carla-Sofia-Centeleghe@users.noreply.github.com>
2023-11-21 21:11:06 -0300

    ejemplo 3 y 4 dispositivos

---
+++ Dispositivo/README.md
@@ -0,0 +1,3 @@
+
+# final-2023-05-17
+Final Mesas Mayo FI
---
+++ Dispositivos2/dispositivo.py
@@ -0,0 +1,7 @@
+class Dispositivo:
+
+    def __init__(self, id: int, nombre="", marca="", tipo= ""):
+        self.id = id
+        self.nombre = nombre
+        self.marca = marca
+        self.tipo = tipo
